599a381f44149bbec4cbe3c4f71b95aefcb2da82
Boaz Rozovski <boazrozo@post.bgu.ac.il>
2019-04-13 13:14:38 +0300

    Implemented PolyTerm

--- src/polycalc/PolyTerm.java
+++ src/polycalc/PolyTerm.java
@@ -6,27 +6,80 @@ public class PolyTerm {
     private Scalar coefficient;
     private int exponent;
 
-    public boolean canAdd(PolyTerm pt) {
-        throw new NotImplementedException();
+    public PolyTerm(Scalar coefficient, int exponent) {
+        if (coefficient == null) {
+            throw new IllegalArgumentException("coefficient is null.");
+        }
+
+        this.coefficient = coefficient;
+        this.exponent = exponent;
     }
 
-    public PolyTerm add(PolyTerm pt) {
-        throw new NotImplementedException();
+    public Scalar getCoefficient() {
+        return coefficient;
     }
 
-    public PolyTerm mul(PolyTerm pt) {
-        throw new NotImplementedException();
+    public void setCoefficient(Scalar coefficient) {
+        this.coefficient = coefficient;
+    }
+
+    public int getExponent() {
+        return exponent;
+    }
+
+    public void setExponent(int exponent) {
+        this.exponent = exponent;
+    }
+
+    public boolean canAdd(PolyTerm polyTerm) {
+        if (polyTerm == null) {
+            throw new IllegalArgumentException("polyTerm is null.");
+        }
+
+        return this.getExponent() == polyTerm.getExponent();
+    }
+
+    public PolyTerm add(PolyTerm polyTerm) {
+        if (polyTerm == null) {
+            throw new IllegalArgumentException("pt is null.");
+        }
+        if (!this.canAdd(polyTerm)) {
+            throw new UnsupportedOperationException("The polyterms must be of the same power.");
+        }
+
+        Scalar newCoefficient = this.getCoefficient().add(polyTerm.getCoefficient());
+        return new PolyTerm(newCoefficient, this.getExponent());
+    }
+
+    public PolyTerm mul(PolyTerm polyTerm) {
+        if (polyTerm == null) {
+            throw new IllegalArgumentException("polyTerm is null.");
+        }
+
+        Scalar newCoefficient = this.getCoefficient().mul(polyTerm.getCoefficient());
+        int newExponent = this.getExponent() + polyTerm.getExponent();
+        return new PolyTerm(newCoefficient, newExponent);
     }
 
     public Scalar evaluate(Scalar scalar) {
-        throw new NotImplementedException();
+        if (scalar == null) {
+            throw new IllegalArgumentException("scalar is null.");
+        }
+
+        return this.getCoefficient().mul(scalar.pow(this.getExponent()));
     }
 
     public PolyTerm derivate() {
-        throw new NotImplementedException();
+        Scalar newCoefficient = this.getCoefficient().mul(this.getExponent());
+        return new PolyTerm(newCoefficient, this.getExponent() - 1);
     }
 
-    public boolean equals(PolyTerm pt) {
-        throw new NotImplementedException();
+    public boolean equals(PolyTerm polyTerm) {
+        if (polyTerm == null) {
+            throw new IllegalArgumentException("polyTerm is null.");
+        }
+
+        return this.getExponent() == polyTerm.getExponent() &
+               this.getCoefficient().equals(polyTerm.getCoefficient());
     }
 }
